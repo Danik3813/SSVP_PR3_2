7f066ce10cd06fc342525606fad32dae54aeed2f
Danil <164644154+Danik3813@users.noreply.github.com>
2024-10-28 11:40:12 +0400

    Add files via upload

---
+++ SSVP_PR3_2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace SSVP_PR3_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string Item = listBox1.SelectedItem.ToString();
+            //TODO: использовать регулярные выражения или функцию contain/indexof
+            /* Regex ruReg = new Regex(@"[а-я]+");
+            MatchCollection match = ruReg.Matches(Item);
+            label3.Text = match.Count.ToString(); */
+            int symbolsCount = 0;
+            string russianAlphabet = "ёйцукенгшщзхъфывапролджэячсмитьбю";
+            for (int i = 0; i < Item.Length; i++)
+            {
+                for (int j = 0; j < russianAlphabet.Length; j++)
+                {
+                    if (Item[i] == russianAlphabet[j]) symbolsCount++;
+                }
+            }
+            label3.Text = symbolsCount.ToString();
+        }
+    }
+}
